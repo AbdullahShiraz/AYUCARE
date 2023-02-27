1c583e52dc3fbccd66bae86b8d1e0b712803da49
Samu_D <51296130+SamudiniD@users.noreply.github.com>
2023-02-27 16:52:43 +0530

    Add files via upload

---
+++ Samudini/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template("index.html")
+
+@app.route('/signup')
+def signup():  # put application's code here
+    return render_template("signup.html")
+
+@app.route('/register')
+def register():  # put application's code here
+    return render_template("register.html")
+
+@app.route('/service')
+def service():  # put application's code here
+    return render_template("service.html")
+
+
+if __name__ == '__main__':
+    app.run()
