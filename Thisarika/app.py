10c8289ef7f0ef5fec79683a710e3cc46012cf7f
ThisarikaThathsarani <108297437+ThisarikaTNA@users.noreply.github.com>
2023-04-03 11:20:17 +0530

    Update app.py

--- Thisarika/app.py
+++ Thisarika/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, session, render_template, request, redirect
-import mysql.connector
+from flask import Flask, session, render_template, request, redirect, flash
+import sqlite3
 import pyrebase
+import bcrypt
+from validate_email import validate_email
 
 app = Flask(__name__)
+app.secret_key = 'ayucare1'
 
-conn = mysql.connector.connect(host='localhost', user='root', password='', database='ayucare')
+conn = sqlite3.connect('ayucare.db')
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS patients ( firstname TEXT NOT NULL, lastname TEXT NOT NULL, age INTEGER NOT NULL, gender VARCHAR NOT NULL, email VARCHAR UNIQUE NOT NULL, address VARCHAR NOT NULL, password VARCHAR NOT NULL, confirmpassword VARCHAR NOT NULL, pastconditions VARCHAR NOT NULL)")
+
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS doctors ( doctor_id INTEGER PRIMARY KEY AUTOINCREMENT, doctor_name TEXT NOT NULL, specialized_field TEXT NOT NULL, contact_number VARCHAR NOT NULL)"
+)
 
 config = {
     'apiKey': "AIzaSyC2XqHQdB9Y0tNppfWAdKYThlOwHGV17cg",
@@ -32,19 +41,34 @@ def signup():  # firebase authendication code
         email = request.form.get('email')
         password = request.form.get('password')
         try:
-            email = request.form['email']
-            password = request.form['password']
+            if not email or not password:
+                return "Please complete all fields"
+
+            if not validate_email(email):
+                raise ValueError("Invalid email address")
+
+            # Hash the password before storing it
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
             new_user = auth.create_user_with_email_and_password(email, password)
             auth.send_email_verification(new_user['idToken'])
-            return render_template('index.html')
-        except:
-            existing_account = 'This email is already used'
-            return render_template('signup.html', exist_message=existing_account)
+
+            flash('Verification email sent. Please check your inbox to complete the registration')
+            return redirect('/')
+
+        except ValueError as e:
+            flash(str(e))
+        except Exception as e:
+            if 'INVALID_PASSWORD' in str(e):
+                flash('check your password')
+            else:
+                flash(str(e))
+
     return render_template("signup.html")
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():         # database connection
+def register():
     if request.method == 'POST':
         firstname = request.form['firstname']
         lastname = request.form['lastname']
@@ -55,29 +79,35 @@ def register():         # database connection
         password = request.form['password']
         confirmpassword = request.form['confirmpassword']
         pastconditions = request.form['pastconditions']
-        cur = conn.cursor()
-        cur.execute(
-            'INSERT INTO patient(firstname,lastname,age,gender,email,address,password,confirmpassword,pastconditions) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)',
-            (firstname, lastname, age, gender, email, address, password, confirmpassword, pastconditions))
-        conn.commit()
 
-        # Firebase Authentication code
-        if password == confirmpassword:
-            try:
-                new_user1 = auth.create_user_with_email_and_password(email, password)
-                auth.send_email_verification(new_user1['idToken'])
-                return render_template('index.html')
-            except:
-                existing_account1 = 'This email is already used'
-                return render_template('register.html', exist_message=existing_account1)
-        else:
-            password_mismatch = 'Passwords do not match'
-            return render_template('register.html', password_error=password_mismatch)
+        # Check if all required fields are filled
+        if not firstname or not lastname or not age or not gender or not email or not address or not password or not confirmpassword or not pastconditions:
+            return "Please fill all the required fields"
+
+        # Check if passwords match
+        if password != confirmpassword:
+            return "check your passwords again"
+
+        with sqlite3.connect("ayucare.db") as conn:
+            c = conn.cursor()
+            c.execute(
+                'INSERT INTO patients(firstname,lastname,age,gender,email,address,password,confirmpassword,pastconditions) values (?,?,?,?,?,?,?,?,?)',
+                (firstname, lastname, age, gender, email, address, password, confirmpassword, pastconditions))
+            conn.commit()
+
+        try:
+            new_user1 = auth.create_user_with_email_and_password(email, password)
+            auth.send_email_verification(new_user1['idToken'])
+            return render_template('index.html')
+        except:
+            existing_account1 = 'This email is already used'
+            return render_template('register.html', exist_message=existing_account1)
+
     return render_template("register.html")
 
 
 @app.route('/service')
-def service():  # put application's code here
+def service():
     return render_template("service.html")
 
 
